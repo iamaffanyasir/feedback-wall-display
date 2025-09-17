be5ddf6017d8d1b9e136d2e77b402b1f70e3a4f7
iamaffanyasir <aafiannu172@gmail.com>
2025-09-17 12:53:35 +0530

    updatev2

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
+const API_BASE_URL =
+  process.env.REACT_APP_API_URL || "http://your-digitalocean-ip";
 
 export const getLatestFeedbacks = async () => {
   const res = await axios.get(`${API_BASE_URL}/feedback?limit=30`);
